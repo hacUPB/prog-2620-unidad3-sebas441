c2bd2365b86b40434a314372f66608181f26bd5c
sarah-m-u <sarah.marquezu@upb.edu.co>
2026-02-25 15:36:24 -0500

    unos archivos más

---
+++ src/actividad 1.py
@@ -0,0 +1,16 @@
+a = 28
+b = 92
+if a > b:
+    print("a es mayor que b")
+else:
+    print("a es menor que b")
+
+ # un bucle que imprime 100 veces la palabra upb
+
+for i in range(100): 
+    print("upb")
+
+
+
+
+        
